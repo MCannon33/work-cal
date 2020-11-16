90cc0f2b7733448ce23bb7a48834091d44f9b341
MCannon33 <marialcannon@gmail.com>
2020-11-16 11:54:52 -0500

    re organize files so that css works

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log (moment())
 $("#currentDay").text("hello");
 //consol.log (today.formatt("MMMM Do YYYY, H:mmA"))
 
+
 $(".saveBtn").on("click", function(){
     console.log("clickedsave")
 });
