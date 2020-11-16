2c17181822f2bd7db67a359f60309953d0a5d943
MCannon33 <marialcannon@gmail.com>
2020-11-16 14:21:05 -0500

    moment date and icons

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //SET THE DATE AT THE TOP OF THE PAGE
 console.log (moment())
-$("#currentDay").text("hello");
-//consol.log (today.formatt("MMMM Do YYYY, H:mmA"))
-
+$("#currentDay").text();
+var now = moment ();
+var date = $(currentDay).text(now.format("dddd, MMMM Do"));
+//(today.formatt("MMMM Do YYYY, H:mmA"))
 
 $(".saveBtn").on("click", function(){
     console.log("clickedsave")
