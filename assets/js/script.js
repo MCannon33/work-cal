5418cbeadfd634c5d230f7fc6fc2bd042195cd19
MCannon33 <marialcannon@gmail.com>
2020-11-16 11:10:01 -0500

    move in code

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//SET THE DATE AT THE TOP OF THE PAGE
+console.log (moment())
+$("#currentDay").text("hello");
+//consol.log (today.formatt("MMMM Do YYYY, H:mmA"))
+
+$(".saveBtn").on("click", function(){
+    console.log("clickedsave")
+});
