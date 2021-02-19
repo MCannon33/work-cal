4296eb7534a6560bed07a47076b5d92836ea5acd
MCannon33 <marialcannon@gmail.com>
2021-02-18 23:32:22 -0500

    fix color so that it adjusts to the time

--- script.js
+++ script.js
@@ -55,6 +55,8 @@ $(document).ready(function () {
   $("#hour-15 .description").val(localStorage.getItem("hour-15"));
   $("#hour-16 .description").val(localStorage.getItem("hour-16"));
   $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  $("#hour-18 .description").val(localStorage.getItem("hour-18"));
+  $("#hour-23 .description").val(localStorage.getItem("hour-23"));
 
   // display current day on page
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
